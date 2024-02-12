054142d44daf64ebdc58d9af1db7e7376a6b6580
fumiyama123 <hiyama0302@icloud.com>
2024-02-12 22:32:26 +0900

    Add:基本処理追加

--- tuktestfrontapp/Components/Pages/Zodiac.razor.cs
+++ tuktestfrontapp/Components/Pages/Zodiac.razor.cs
@@ -1,14 +1,37 @@
-﻿namespace tuktestfrontapp.Components.Pages
+﻿using System.Runtime.Serialization.Json;
+using System.Text.Json;
+
+
+namespace tuktestfrontapp.Components.Pages
 {
     public partial class Zodiac
     {
         public OrientalZodiacModel orientalZodiac = new();
         public string? year;
 
-        public void ShowYear()
+        public async Task ShowYear()
         {
-            orientalZodiac.StatusCode = 200;
-            orientalZodiac.Value = "辰";
+            var parameters = new Dictionary<string, string>()
+                {
+                    { "code", "yg3fmpfzMBJA2eIBmAvlIJHZaxo7v0b8K6-yRREsM9i-AzFuHi4qlg==" },
+                    { "year", year! }
+                };  
+
+            using (var client = new HttpClient())
+            {
+                var response =
+                    await client.GetAsync($"https://tuktestfunc.azurewebsites.net/api/FunctionOrientalZodiac?{await new FormUrlEncodedContent(parameters).ReadAsStringAsync()}");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    orientalZodiac = JsonSerializer.Deserialize<OrientalZodiacModel>(jsonResponse)!;
+                }
+                else
+                {
+                    orientalZodiac.Value = "API通信に失敗しました。";
+                }
+            }
         }
     }
 }
